0c3430e8ea00742e19fa39576b202855112cf88e
Diogo Autilio <diautilio@gmail.com>
2014-07-16 23:05:53 -0300

    --Removed log

--- travis-environment.sh
+++ travis-environment.sh
@@ -5,8 +5,3 @@ set -ex
 export "PSPDEV=$TRAVIS_BUILD_DIR/pspdev"
 export "PSPSDK=$PSPDEV/psp"
 export "PATH=$PATH:$PSPDEV/bin:$PSPSDK/bin"
-
-# let's dump some info to debug a bit
-echo PSPDEV = $PSPDEV
-echo ls = `ls`
-echo pwd = `pwd`
