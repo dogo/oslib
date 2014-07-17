5f033d643e63cffed3e1ff1117bbd5ad50bc8e35
Diogo Autilio <diautilio@gmail.com>
2014-07-16 22:52:02 -0300

    debug info

--- travis-environment.sh
+++ travis-environment.sh
@@ -2,12 +2,12 @@
 set -ex
 
 # Add PSP environment variable
+echo $TRAVIS_BUILD_DIR
 export PSPDEV=`pwd`/pspsdk
 export PSPSDK=$PSPDEV/bin
 export PATH=$PSPSDK:$PATH
 
 # let's dump some info to debug a bit
 echo PSPDEV = $PSPDEV
-echo psp-config = `psp-config --psp-prefix`
 echo ls = `ls`
 echo pwd = `pwd`
