fdea45e4148431f2b416a5771990d61b2cd18439
Diogo Autilio <diautilio@gmail.com>
2024-06-05 10:59:02 -0300

    Replace Travis CI with Github Actions

--- travis/travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-set -ex
-
-# Add PSP environment variable
-export "PSPDEV=$TRAVIS_BUILD_DIR/pspdev"
-export "PSPSDK=$PSPDEV/psp"
-export "PATH=$PATH:$PSPDEV/bin:$PSPSDK/bin"
-
-make
--- travis/travis-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brew update
-brew install mpfr
-brew install libelf
-brew install libmpc
-brew install libusb
