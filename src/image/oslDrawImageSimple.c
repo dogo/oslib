278b2f654891cbf68371e6555a7a584c1b6282e1
Diogo Autilio <diautilio@gmail.com>
2024-08-30 06:43:50 -0300

    Add comments to clarify the purpose of each section, making the code easier to understand Improved indentation and alignment for readability

--- src/image/oslDrawImageSimple.c
+++ src/image/oslDrawImageSimple.c
@@ -1,40 +1,46 @@
 #include "oslib.h"
 
-/*
-	Marche pas, le pixelformat doit être le même que le buffer
-inline void DrawImageFast(IMAGE *img)
-{
-	sceGuCopyImage(GU_PSM_4444,0,0,img->sizeX,img->sizeY,img->sizeX,img->data,img->x,img->y,512,(void*)(0x04000000+(u32)g_curDrawBuf));
-}*/
+/**
+ * Draws an image at its specified coordinates using a simple blit.
+ * This function does not account for any rotation or scaling beyond the basic stretch.
+ * It is optimized for simplicity and minimal overhead.
+ *
+ * @param img The image to draw.
+ */
+void oslDrawImageSimple(OSL_IMAGE *img) {
+    // Pointer to the vertex array for rendering the image
+    OSL_UVFLOAT_VERTEX *vertices;
 
+    // Get the image's position
+    int x = img->x;
+    int y = img->y;
 
-void oslDrawImageSimple(OSL_IMAGE *img)				{
-		OSL_UVFLOAT_VERTEX *vertices;
-		int x = img->x, y = img->y;
-		//Very little overhead to support the rotation center but anyway... this routine was meant to be very simple :p
-//		int x = img->x - img->centerX, y = img->y - img->centerY;
+    // Set the texture to be used for rendering
+    oslSetTexture(img);
 
-		// do a striped blit (takes the page-cache into account)
-		oslSetTexture(img);
-		if (oslImageGetAutoStrip(img))			{
-			if (oslVerifyStripBlit(img))
-				return;
-		}
+    // If auto-strip is enabled, verify and handle it
+    if (oslImageGetAutoStrip(img)) {
+        if (oslVerifyStripBlit(img))
+            return;  // Return early if strip blit is verified and completed
+    }
 
-		vertices = (OSL_UVFLOAT_VERTEX*)sceGuGetMemory(2 * sizeof(OSL_UVFLOAT_VERTEX));
+    // Allocate memory for two vertices
+    vertices = (OSL_UVFLOAT_VERTEX*)sceGuGetMemory(2 * sizeof(OSL_UVFLOAT_VERTEX));
 
-		vertices[0].u = img->offsetX0;
-		vertices[0].v = img->offsetY0;
-		vertices[0].x = x;
-		vertices[0].y = y;
-		vertices[0].z = 0;
+    // Define the top-left vertex of the image
+    vertices[0].u = img->offsetX0;
+    vertices[0].v = img->offsetY0;
+    vertices[0].x = x;
+    vertices[0].y = y;
+    vertices[0].z = 0;
 
-		vertices[1].u = img->offsetX1;
-		vertices[1].v = img->offsetY1;
-		vertices[1].x = x + img->stretchX;
-		vertices[1].y = y + img->stretchY;
-		vertices[1].z = 0;
+    // Define the bottom-right vertex of the image
+    vertices[1].u = img->offsetX1;
+    vertices[1].v = img->offsetY1;
+    vertices[1].x = x + img->stretchX;
+    vertices[1].y = y + img->stretchY;
+    vertices[1].z = 0;
 
-		sceGuDrawArray(GU_SPRITES,GU_TEXTURE_32BITF|GU_VERTEX_16BIT|GU_TRANSFORM_2D,2,0,vertices);
+    // Draw the image as a 2D sprite
+    sceGuDrawArray(GU_SPRITES, GU_TEXTURE_32BITF | GU_VERTEX_16BIT | GU_TRANSFORM_2D, 2, 0, vertices);
 }
-
