cbe94a3078f9196e27d54d75603aed54da4cff44
Diogo Autilio <diautilio@gmail.com>
2024-09-10 00:01:53 -0300

    Code formatting oslHandleLoadNoFailError

--- src/oslHandleLoadNoFailError.c
+++ src/oslHandleLoadNoFailError.c
@@ -3,16 +3,19 @@
 void osl_noFailDefaultCallback(const char *filename, u32 reserved);
 void (*osl_noFailCallback)(const char *filename, u32 reserved) = osl_noFailDefaultCallback;
 
-void osl_noFailDefaultCallback(const char *filename, u32 reserved)		{
-	oslFatalError("Unable to locate the following file: %s\nPlease reinstall the application and read the instructions carefully.", filename);
+void osl_noFailDefaultCallback(const char *filename, u32 reserved) {
+    oslFatalError("Unable to locate the following file: %s\nPlease reinstall the application and read the instructions carefully.", filename);
 }
 
-void oslHandleLoadNoFailError(const char *filename)		{
-	//Add 1 to enable error trapping
-	if (osl_noFail & 1)		{
-		osl_noFailCallback(filename, 0);
-		//Add 2 to avoid quitting after the error message
-		if (!(osl_noFail & 2))
-			oslQuit();
-	}
+void oslHandleLoadNoFailError(const char *filename) {
+    // Check if error trapping is enabled (bitwise AND with 1)
+    if (osl_noFail & 1) {
+        osl_noFailCallback(filename, 0);
+
+        // Check if the application should quit after displaying the error
+        // (bitwise AND with 2, if not set, proceed to quit)
+        if (!(osl_noFail & 2)) {
+            oslQuit();
+        }
+    }
 }
